df2a35da6675a4f529a78f01febd35b274f3c6ba
Jdar15 <83215247+Jdar15@users.noreply.github.com>
2021-05-28 14:29:00 +0200

    Update routes.py

--- Projet_python/handlers/routes.py
+++ Projet_python/handlers/routes.py
@@ -6,7 +6,7 @@ def configure_routes(app):
 
     @app.route('/')
     def hello_world():
-        return 'Coucou, le monde !'
+        return 'Hello, World!'
        
     @app.route('/post/test', methods=['POST'])
     def receive_post():
